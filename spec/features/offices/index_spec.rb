f5b8a63091502b66a6ebc3f83bbc38c13d0fe407
Brisa Garcia <brisagarciaglz@gmail.com>
2021-03-23 18:32:33 -0600

    Add user stroy 1

---
+++ spec/features/offices/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor,", type: :feature do
+  describe "When I visit the Office index" do
+    before :each do
+      @rmnp_office = Office.create(name: 'RMNP Office', capacity: 200, first_aid: true )
+      @moab_office = Office.create(name: 'Alamosa Office', capacity: 50, first_aid: false )
+    end
+
+    it "Then I see the name of each office record in the system" do
+      visit '/offices'
+
+      # save_and_open_page
+
+      expect(page).to have_content(@rmnp_office.name)
+      expect(page).to have_content(@moab_office.name)
+    end
+  end
+end

15104b3800a761872d4a0cb0ee4118292c48a9d7
joemecha <jwmecha@gmail.com>
2021-03-25 17:58:33 -0500

    Add new park form

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -6,4 +6,19 @@ class ParksController < ApplicationController
   def show
     @park = Park.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    park = Park.new(park_params)
+    park.save
+    redirect_to '/parks'
+  end
+
+  private
+
+  def park_params
+    params.permit(:name, :capacity, :permit_required)
+  end
 end
---
+++ spec/features/parks/new_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor", type: :feature do
+  describe "I am directed to New Form" do
+    it "I can create a new park" do
+      visit '/parks/new'
+
+      fill_in "Name", with: "Park 1"
+      fill_in "Capacity", with: "23"
+      # check "Permit_required"
+
+      click_on "Create Park"
+
+      expect(current_path).to eq("/parks")
+      expect(page).to have_content("Park 1")
+      expect(page).to have_content(23)
+      # expect(page).to have_content(false)
+    end
+  end
+end

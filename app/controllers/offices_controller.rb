f5a652306753d5915af621f3118a612367d3edce
Brisa Garcia <brisagarciaglz@gmail.com>
2021-03-25 16:40:50 -0600

    Add new and create methods.  ad private params)

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -6,4 +6,24 @@ class OfficesController < ApplicationController
   def show
     @office = Office.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    office = Office.new(office_params)
+      # name: params[:office][:name],
+      # capacity: params[:office][:capacity],
+      # first_aid: params[:office][:first_aid]
+      # })
+
+      office.save
+      redirect_to '/offices'
+  end
+
+  private
+
+  def office_params
+    params.permit(:name, :capacity, :first_aid)
+  end
 end

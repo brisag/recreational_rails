7acde97baae7b3642adcce7b50afb40c1648c64b
Brisa Garcia <brisagarciaglz@gmail.com>
2021-03-26 11:50:49 -0600

    Add form

---
+++ spec/features/parks/edit_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor", type: :feature do
+  describe "I am directed to Update Form" do
+    it "I can edit a Park" do
+      @grand_canyon = Park.create!(name: 'GRCA Park', capacity: 30, permit_required: false )
+
+      visit "/parks/#{@grand_canyon.id}/edit"
+
+      fill_in "Name", with: "Little Canyon"
+      fill_in "Capacity", with: "44"
+      # check "First Aid"
+
+      click_on "Update Park"
+
+      expect(current_path).to eq("/parks/#{@grand_canyon.id}")
+      expect(page).to have_content("Little Canyon")
+      expect(page).to have_content(44)
+      # expect(page).to have_content(false)
+    end
+  end
+end

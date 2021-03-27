a62fd80d5a20b01bdeef24db5aa837264686957c
joemecha <jwmecha@gmail.com>
2021-03-26 19:17:08 -0500

    Add Create Trail feature and spec

--- app/controllers/office_trails_controller.rb
+++ app/controllers/office_trails_controller.rb
@@ -2,4 +2,20 @@ class OfficeTrailsController < ApplicationController
   def index
     @office = Office.find(params[:id])
   end
+
+  def new
+    @office = Office.find(params[:id])
+  end
+
+  def create
+    @office = Office.find(params[:id])
+    @office.trails.create!(trail_params)
+    redirect_to "/offices/#{@office.id}/trails"
+  end
+
+  private
+
+  def trail_params
+    params.permit(:name, :elevation, :dogs_allowed)
+  end
 end

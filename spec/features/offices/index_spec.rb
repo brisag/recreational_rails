1ee4adb338f6437296488c713e28d659b6fb7e30
Brisa Garcia <brisagarciaglz@gmail.com>
2021-03-31 17:29:02 -0600

    Add test to test features for AR

--- spec/features/offices/index_spec.rb
+++ spec/features/offices/index_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe "As a visitor,", type: :feature do
       @rmnp_office = Office.create!(name: 'RMNP Office', capacity: 200, first_aid: true )
       @moab_office = Office.create!(name: 'Alamosa Office', capacity: 50, first_aid: false )
       @zion_office = Office.create!(name: 'Zion Office', capacity: 250, first_aid: true )
+
+      @dream_lake = @rmnp_office.trails.create(name: 'Dream Lake', elevation: 425, dogs_allowed: true)
+      @bear_lake = @rmnp_office.trails.create(name: 'Bear Lake Loop', elevation: 20, dogs_allowed: true)
+      @delicate_arch = @moab_office.trails.create(name: 'Delicate Arch', elevation: 480, dogs_allowed: true)
+      @park_avenue = @moab_office.trails.create(name: 'Park Avenue', elevation: 320, dogs_allowed: true)
+      @double_arch = @moab_office.trails.create(name: 'Double Arch', elevation: 0, dogs_allowed: true)
+      @weeping_rock = @zion_office.trails.create(name: 'Weeping Rock', elevation: 98, dogs_allowed: true)
     end
 
     it "Then I see the name of each office record in the system" do
@@ -31,6 +38,45 @@ RSpec.describe "As a visitor,", type: :feature do
         click_link "Update Office"
         expect(current_path).to eq("/offices/#{@rmnp_office.id}/edit")
       end
+    end
+
+    it 'Has a form to search All Parks' do
+      visit '/offices'
+
+
+      fill_in with: "RMNP Office"
+
+      click_on "Search"
+
+      expect(current_path).to eq("/offices")
+
+      expect(page).to have_content(@rmnp_office.name)
+      expect(page).to_not have_content(@zion_office.name)
+    end
+
+    it 'Has a form to partially search All Parks' do
+      visit '/offices'
+
+
+      fill_in with: "Zi"
+
+      click_on "Search"
+
+      expect(current_path).to eq("/offices")
+
+      expect(page).to have_content(@zion_office.name)
+      expect(page).to_not have_content(@rmnp_office.name)
+    end
+
+    it "has a link to sort by most trails to least trails" do
+      visit '/offices'
+
+      expect(page).to have_link("Sort by Offices with the most trails to least")
+
+      click_link "Sort by Offices with the most trails to least"
+
+      expect(current_path).to eq("/offices")
+      expect(@moab_office.name).to appear_before(@zion_office.name)
     end
   end
 end
--- spec/features/parks/index_spec.rb
+++ spec/features/parks/index_spec.rb
@@ -5,8 +5,13 @@ RSpec.describe "As a visitor", type: :feature do
     before :each do
       @grand_canyon = Park.create!(name: 'GRCA Park', capacity: 30, permit_required: false )
       @sand_dunes= Park.create!(name: 'Sand Dunes', capacity: 25, permit_required: false )
-      @crater_lake= Park.create!(name: 'Sand Dunes', capacity: 25, permit_required: false )
-
+      @crater_lake= Park.create!(name: 'Crater Lake', capacity: 20, permit_required: false )
+      @fishing = @grand_canyon.programs.create!(name: 'Grand Fishing Online', num_of_participants: 100, virtual: true)
+      @rafting = @grand_canyon.programs.create!(name: 'Raft the Waves', num_of_participants: 8, virtual: false)
+      @hiking = @grand_canyon.programs.create!(name: 'Grand Descent', num_of_participants: 30, virtual: false)
+      @sand_boarding = @sand_dunes.programs.create!(name: 'Sandboard', num_of_participants: 6, virtual: false)
+      @hot_springs = @sand_dunes.programs.create!(name: 'Hot Spring Nights', num_of_participants: 12, virtual: false)
+      @conspiracies = @crater_lake.programs.create!(name: 'What Crater?', num_of_participants: 6, virtual: false)
       visit '/parks'
     end
 
@@ -30,6 +35,39 @@ RSpec.describe "As a visitor", type: :feature do
         click_link "Update Park"
         expect(current_path).to eq("/parks/#{@sand_dunes.id}/edit")
       end
+    end
+
+    it 'Has a form to search All Parks' do
+
+      fill_in with: "Crater Lake"
+
+      click_on "Search"
+
+      expect(current_path).to eq("/parks")
+
+      expect(page).to have_content(@crater_lake.name)
+      expect(page).to_not have_content(@sand_dunes.name)
+    end
+
+    it 'Has a form to partially search All Parks' do
+
+      fill_in with: "Sand"
+
+      click_on "Search"
+
+      expect(current_path).to eq("/parks")
+
+      expect(page).to have_content(@sand_dunes.name)
+      expect(page).to_not have_content(@crater_lake.name)
+    end
+
+    it "has a link to sort by most programs to least programs" do
+      expect(page).to have_link("Sort by Parks with the most programs to least")
+
+      click_link "Sort by Parks with the most programs to least"
+      expect(current_path).to eq("/parks")
+
+      expect(@grand_canyon.name).to appear_before(@crater_lake.name)
     end
   end
 end

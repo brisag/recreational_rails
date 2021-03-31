602749d6c8f2f785a0b94eabfed38802d30541e0
joemecha <jwmecha@gmail.com>
2021-03-30 22:43:03 -0500

    Add validation to office and trail models only

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,4 +1,6 @@
 class Trail < ApplicationRecord
+  validates :name, :elevation, presence: true
+  validates :dogs_allowed, inclusion: [true, false]
   belongs_to :office
 
   def self.true_dogs_allowed
